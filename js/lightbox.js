f0f5f7d58f001e59fc0fae25f0b83aaeb5d62246
Roberto Rivera <robertoriveracreative@gmail.com>
2019-07-10 19:09:08 -0400

    This is the final push for the whole website

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,17 +1,18 @@
-var $thumbs = $('.thumbnails');
-var $lightboxIMG = $('.lightbox-img');
-var $lightbox = $('.lightbox');
-var $btnClose = $('.btn-close');
+var $thumbs = $('.thumbnails'); // thumbnails
+var $lightboxIMG = $('.lightbox-img'); // lightbox image
+var $lightbox = $('.lightbox'); // lightbox container
+var $btnClose = $('.btn-close'); // close button
 
+// This triggers the lightbox to hide or display
 $thumbs.on('click', 'a', function (e) {
-    e.preventDefault();
-    var $big = $(this).attr('href');
+    e.preventDefault(); // prevents default action of the a tag
+    var $big = $(this).attr('href'); // get the href from the thumbnail a
     //console.log(big);
-    $lightboxIMG.attr('src', $big);
-    $lightbox.attr('data-state', 'visible');
+    $lightboxIMG.attr('src', $big); // change the source attribute for the lightbox image
+    $lightbox.attr('data-state', 'visible'); // change the lightbox from hidden to visible
 });
 
+// Triggers the close button
 $btnClose.on('click', function () {
-    $lightbox.attr('data-state', 'hidden');
-
+    $lightbox.attr('data-state', 'hidden'); // change the lightbox from visible to hidden
 });
