b0850d32342296d1a74fe02530c410e71a7259f2
Roberto Rivera <robertoriveracreative@gmail.com>
2019-07-17 20:09:52 -0400

    javascript update

--- js/lightbox.js
+++ js/lightbox.js
@@ -3,14 +3,20 @@ var $lightboxIMG = $('.lightbox-img'); // lightbox image
 var $lightbox = $('.lightbox'); // lightbox container
 var $btnClose = $('.btn-close'); // close button
 
+function onDesktop(){
+    let mql = window.matchMedia('(max-width: 540px)'); 
+    return(!mql.matches) 
+} //mobile option
 // This triggers the lightbox to hide or display
 $thumbs.on('click', 'a', function (e) {
     e.preventDefault(); // prevents default action of the a tag
     var $big = $(this).attr('href'); // get the href from the thumbnail a
     //console.log(big);
     $lightboxIMG.attr('src', $big); // change the source attribute for the lightbox image
-    $lightbox.attr('data-state', 'visible'); // change the lightbox from hidden to visible
-});
+    if(onDesktop()){
+        $lightbox.attr('data-state', 'visible'); // change the lightbox from hidden to visible   
+    }
+  });
 
 // Triggers the close button
 $btnClose.on('click', function () {
